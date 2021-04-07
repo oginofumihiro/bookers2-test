452a371eea4b2cdff835d971185d770a1aa389c4
oginofumihiro <fumi1127ogino@gmail.com>
2021-04-07 14:53:29 +0000

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,7 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  has_many :relationships, class_name: "relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followers, through: :relationships, source: :follow
+  has_many :followeds, through: :reverse_of_relationships, source: :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_06_092649) do
+ActiveRecord::Schema.define(version: 2021_04_07_103606) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "body"
@@ -39,6 +39,13 @@ ActiveRecord::Schema.define(version: 2021_04_06_092649) do
     t.text "comment"
     t.integer "book_id"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
